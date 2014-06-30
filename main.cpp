b334d6b0c77236aee94832730fcf6f304091b7c6
Tiago Pereira <tiagogomespereira@gmail.com>
2014-06-30 12:44:50 -0300

    funfa

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
-int main(){
-	
-	cout << "Hello GPS world!" << endl;
+//Aresta
+typedef struct _caminho{	
+	char destino;
+	int distancia;
+}Caminho;
 
+
+//Grafo
+map< char, vector<Caminho> > mapa;
+
+void inserirCaminho(char origem, char destino, int distancia){
+	Caminho temp;
+	temp.destino = destino;
+	temp.distancia = distancia;
+
+	mapa[origem].push_back(temp);
+
+	temp.destino = origem;
+	temp.distancia = distancia;
+	mapa[destino].push_back(temp);
+}
+
+int main(){
+
+	char origem, destino;
+	int distancia;
+
+	while(cin >> origem >> destino >> distancia){
+		inserirCaminho(origem,destino,distancia);
+	}
+
+	map< char, vector<Caminho> >::iterator it; 
+	for(it = mapa.begin();it != mapa.end();it++){
+		vector<Caminho> adjacencias = it->second;
+		vector<Caminho>::iterator vit;
+		for(vit = adjacencias.begin(); vit != adjacencias.end() ; vit++){
+			cout << it->first << " " << vit->destino << " " << vit->distancia << endl;
+		}
+		cout << endl;
+	}
 	return 0;
 }
